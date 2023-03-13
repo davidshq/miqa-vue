ac5a9b12142ea287c0999ea9f74d9ac9c5338bf7
Dave Mackey <dave@davemackey.net>
2023-03-13 12:05:04 -0400

    Uses VTK.js Vue tutorial to render a simple cone

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,146 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div>
-</template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+    <div>
+      <div ref="vtkContainer"></div>
+      <table class="controls">
+        <tbody>
+          <tr>
+            <td>
+              <select
+                style="width: 100%"
+                :value="representation"
+                @change="setRepresentation($event.target.value)"
+              >
+                <option value="0">Points</option>
+                <option value="1">Wireframe</option>
+                <option value="2">Surface</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input
+                type="range"
+                min="4"
+                max="80"
+                :value="coneResolution"
+                @input="setConeResolution($event.target.value)"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, unref, onMounted, onBeforeUnmount, watchEffect } from 'vue';
+  
+  import '@kitware/vtk.js/Rendering/Profiles/Geometry';
+  
+  import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
+  
+  import vtkActor           from '@kitware/vtk.js/Rendering/Core/Actor';
+  import vtkMapper          from '@kitware/vtk.js/Rendering/Core/Mapper';
+  import vtkConeSource      from '@kitware/vtk.js/Filters/Sources/ConeSource';
+  
+  export default {
+    name: 'HelloWorld',
+  
+    setup() {
+      console.log('Running setup');
+      const vtkContainer = ref(null);
+      const context = ref(null);
+      const coneResolution = ref(6);
+      const representation = ref(2);
+  
+      function setConeResolution(res) {
+        coneResolution.value = Number(res);
+      }
+  
+      function setRepresentation(rep) {
+        representation.value = Number(rep);
+      }
+  
+      watchEffect(() => {
+        console.group('Running watchEffect');
+        const res = unref(coneResolution);
+        const rep = unref(representation);
+        console.debug('context.value', context.value);
+        if (context.value) {
+          const { actor, coneSource, renderWindow } = context.value;
+          console.debug('coneSource', coneSource);
+          coneSource.setResolution(res);
+          console.debug('actor', actor);
+          actor.getProperty().setRepresentation(rep);
+          console.debug('renderWindow', renderWindow);
+          renderWindow.render();
+          console.groupEnd();
+        }
+      });
+  
+      onMounted(() => {
+        console.group('Running onMounted');
+        if (!context.value) {
+            console.debug('vtkContainer.value', vtkContainer.value);
+          const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+            rootContainer: vtkContainer.value,
+          });
+          const coneSource = vtkConeSource.newInstance({ height: 1.0 });
+  
+          const mapper = vtkMapper.newInstance();
+          mapper.setInputConnection(coneSource.getOutputPort());
+  
+          const actor = vtkActor.newInstance();
+          actor.setMapper(mapper);
+  
+          const renderer = fullScreenRenderer.getRenderer();
+          const renderWindow = fullScreenRenderer.getRenderWindow();
+  
+          renderer.addActor(actor);
+          renderer.resetCamera();
+          renderWindow.render();
+  
+          context.value = {
+            fullScreenRenderer,
+            renderWindow,
+            renderer,
+            coneSource,
+            actor,
+            mapper,
+          };
+        }
+        console.groupEnd();
+      });
+  
+      onBeforeUnmount(() => {
+        console.log('Running onBeforeUnmount');
+        if (context.value) {
+          const { fullScreenRenderer, coneSource, actor, mapper } = context.value;
+          actor.delete();
+          mapper.delete();
+          coneSource.delete();
+          fullScreenRenderer.delete();
+          context.value = null;
+        }
+      });
+  
+      return {
+        vtkContainer,
+        setRepresentation,
+        setConeResolution,
+        coneResolution,
+        representation,
+      };
+    }
   }
-}
-</style>
+  </script>
+  
+  <style scoped>
+  .controls {
+    position: absolute;
+    top: 25px;
+    left: 25px;
+    background: white;
+    padding: 12px;
+  }
+  </style>
