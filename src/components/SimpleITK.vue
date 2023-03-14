0fe8b0178fa4385ec6c1aa6762c1326ccca3785f
Dave Mackey <dave@davemackey.net>
2023-03-14 10:39:09 -0400

    Use official Vite example to implement basic ITK read

--- src/components/SimpleITK.vue
+++ src/components/SimpleITK.vue
@@ -1,8 +1,49 @@
-<script>
-import vtkITKImageReader from "@kitware/vtk.js/IO/Misc/ITKImageReader";
-import { readImageArrayBuffer } from "itk-wasm";
+<template>
+  <!-- Input selector -->
+  <div>
+    <label>Select image or mesh:</label>
+    <input name="inputFile" type="file" />
+  </div>
 
+  <!-- File information -->
+  <textarea readonly name="fileInformation">File information...</textarea>
+</template>
 
-vtkITKImageReader.setReadImageArrayBufferFromITK(readImageArrayBuffer);
+<script setup>
+import { onMounted } from 'vue';
+import { readFile } from 'itk-wasm'
+import curry from 'curry'
 
+const outputFileInformation = curry(function outputFileInformation (
+  outputTextArea,
+  event
+) {
+  console.log('Running outputFileInformation');
+  outputTextArea.textContent = 'Loading...'
+
+  const dataTransfer = event.dataTransfer
+  const files = event.target.files || dataTransfer.files
+
+  return readFile(null, files[0])
+  .then(function ({ image, mesh, webWorker }) {
+    webWorker.terminate()
+    const imageOrMesh = image || mesh
+
+    function replacer (key, value) {
+      if (!!value && value.byteLength !== undefined) {
+        return String(value.slice(0, 6)) + '...'
+      }
+      return value
+    }
+    outputTextArea.textContent = JSON.stringify(imageOrMesh, replacer, 4)
+  })
+})
+
+onMounted(() => {
+  console.log('Running onMounted');
+  const outputTextArea = document.querySelector('textarea')
+  const handleFile = outputFileInformation(outputTextArea)
+  const fileInput = document.querySelector('input')
+  fileInput.addEventListener('change', handleFile)
+})
 </script>
