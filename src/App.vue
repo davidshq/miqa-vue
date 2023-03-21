15ad9970593262b3909f7d1d38871a612013f085
Dave Mackey <dave@davemackey.net>
2023-03-21 11:09:09 -0400

    Remove about page

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/simpleitk">SimpleITK</RouterLink>
         <RouterLink to="/simplevtk">SimpleVTK</RouterLink>
         <RouterLink to="/simpleitkvtk">SimpleITKVTK</RouterLink>
